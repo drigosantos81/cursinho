ba6109ad88ef6fb5ae8602434fa3940a56bf6f37
Rodrigo Emanuel <drigosantos@gmail.com>
2020-06-08 12:27:15 -0300

    Criado arquivo de conexão com o Banco de Dados. Atualizado configuração dos Controllers.

--- src/app/controllers/professores.js
+++ src/app/controllers/professores.js
@@ -1,143 +1,50 @@
-const fs = require('fs');
-const data = require('../../lib/data.json');
 const { age, date } = require('../../lib/utils');
 const Intl = require('intl');
 
-// index
-exports.index = function(req, res) {
-    return res.render("professores/index", { professores: data.professores });
-};
-
-exports.create = function(req, res) {
-    return res.render("professores/create");
-}
-
-// create - POST
-exports.post = function(req, res) {
+module.exports = {
+    index(req, res) {
+        return res.render("professores/index");
+    },
     
-    const keys = Object.keys(req.body);
+    create(req, res) {
+        return res.render("professores/create");
+    },
+    
+    post(req, res) {
+        const keys = Object.keys(req.body);
 
-    for (key of keys) {
-        if (req.body[key] == "") {
-            return res.send("Por favor, preencha todos os campos.");
+        for (key of keys) {
+            if (req.body[key] == "") {
+                return res.send("Por favor, preencha todos os campos.");
+            }
         }
-    }
 
-    let { avatar_url, name, birth, estudo, aula, services } = req.body;
-
-    birth = Date.parse(birth);
-    const created_at = Date.now();
-    const id = Number(data.professores.length + 1);
-
-    data.professores.push({
-        id, 
-        avatar_url, 
-        name, 
-        birth, 
-        aula,
-        estudo,
-        services, 
-        created_at
-    });
-
-    fs.writeFile("./src/lib/data.json", JSON.stringify(data, null, 2), function(err) {
-        if (err) return res.send("Erro ao salvar o arquivo.");
-
-        return res.redirect("/professores");
-    });
-};
-
-// show
-exports.show = function(req, res) {
-    const { id } = req.params;
-
-    const foundProfessor = data.professores.find(function(professor) {
-        return id == professor.id;
-    });
+       return;
+    },
     
-    if (!foundProfessor){
-        return res.send("Professor não encontrado.");
-    }
-
-    const professor = {
-        ...foundProfessor,
-        age: age(foundProfessor.birth),
-        services: foundProfessor.services.split(","),
-        created_at: new Intl.DateTimeFormat("pt-BR").format(foundProfessor.created_at),
-    }
-
-    return res.render("professores/show", { professor });
-};
-
-// edit
-exports.edit = function(req, res) {
-    const { id } = req.params;
-
-    const foundProfessor = data.professores.find(function(professor) {
-        return id == professor.id;
-    });
+    show(req, res) {
+        return;
+    },
     
-    if (!foundProfessor){
-        return res.send("Professor não encontrado.");
-    }
-
-    const professor = {
-        ...foundProfessor,
-        birth: date(foundProfessor.birth),
-    }
+    edit(req, res) {
+        return;
+    },
     
-    return res.render("professores/edit", { professor });
+    put(req, res) {
+        const { id } = req.body;
+        let index = 0;
+
+        const foundProfessor = data.professores.find(function(professor, foundIndex) {
+            if (id == professor.id) {
+                index = foundIndex;
+                return true;
+            };
+        });
+        
+       return;
+    },
+    
+    delete(req, res) {
+        return;
+    }
 }
-
-// update - PUT
-exports.put = function(req, res) {
-    const { id } = req.body;
-    let index = 0;
-
-    const foundProfessor = data.professores.find(function(professor, foundIndex) {
-        if (id == professor.id) {
-            index = foundIndex;
-            return true;
-        };
-    });
-    
-    if (!foundProfessor){
-        return res.send("Professor não encontrado.");
-    }
-
-    const professor = {
-        ...foundProfessor,
-        ...req.body,
-        birth: Date.parse(req.body.birth),
-        id: Number(req.body.id),
-    }
-
-    data.professores[index] = professor;
-
-    fs.writeFile("./src/lib/data.json", JSON.stringify(data, null, 2), function(err) {
-        if (err) {
-            return res.send("Erro ao salvar o arquivo.");
-        }
-        return res.redirect(`/professores/${id}`);
-    })
-
-};
-
-// delete - DELETE
-exports.delete = function(req, res) {
-    const { id } = req.body;
-
-    const filteredProfessores = data.professores.filter(function(professor) {
-        return professor.id != id;
-    });
-
-    data.professores = filteredProfessores;
-
-    fs.writeFile("../../lib/data.json", JSON.stringify(data, null, 2), function(err) {
-        if (err) {
-            return res.send("Erro ao salvar o arquivo.");
-        };
-    });
-
-    return res.redirect("/professores");
-};
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,5 +1,5 @@
 module.exports = {
-    age: function(timestamp) {
+    age(timestamp) {
         
         const today = new Date();
         const birthDate = new Date(timestamp);
@@ -14,7 +14,7 @@ module.exports = {
         return age;
     },
 
-    date: function(timestamp) {
+    date(timestamp) {
 
         const date = new Date(timestamp);
 
@@ -26,7 +26,7 @@ module.exports = {
         return (`${year}-${month}-${day}`);
     },
 
-    birthDay: function(timestamp) {
+    birthDay(timestamp) {
 
         const date = new Date(timestamp);
 
